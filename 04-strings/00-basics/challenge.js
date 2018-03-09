7e2198b259599a7cc16698df73fa6576f1d49258
Emrin <emrin95@gmail.com>
2018-03-09 15:57:13 +0100

    yeah alright we get it

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,5 +1,5 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+    return "Hello " + firstName +",\n\n"+message+"\n\nSincerely,\n"+senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
