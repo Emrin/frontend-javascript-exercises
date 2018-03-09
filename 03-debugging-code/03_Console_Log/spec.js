f27800a7021836b1f13abe5c944b2efd47c87f34
Emrin <emrin95@gmail.com>
2018-03-09 15:51:32 +0100

    doen

--- 03-debugging-code/03_Console_Log/spec.js
+++ 03-debugging-code/03_Console_Log/spec.js
@@ -1,12 +1,12 @@
 var expect    = require("chai").expect;
 var challenge = require("./challenge.js");
 
-describe("add", function() {
+describe("add", function describe() {
   it("should be defined", function() {
     expect(challenge.add).to.exist;
   });
 
   it("sums the two numbers", function() {
     expect(challenge.add(1,2)).to.equal(3);
-  });
+  })
 });
