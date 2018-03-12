8c66bd95bf5107833998632139c8f4ef69d6329d
Emrin <emrin95@gmail.com>
2018-03-12 19:53:17 +0100

    damn I forgot

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -3,17 +3,22 @@ module.exports.formLetter = function(firstName, senderName, message) {
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+    if (bigString == ""){
+        return 0
+    }
+    return bigString.substring(startA, endA) + bigString.substring(startB, endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+    return text.search(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+    return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
+    //return text.substring((text.indexOf(searchString)+searchString.length),text.search(searchString));
+    return text.substring(text.search(searchString)+searchString.length, text.lastIndexOf(searchString));
 
 };
